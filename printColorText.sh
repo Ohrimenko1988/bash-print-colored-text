372ccbdf338988666fac1aaabfbcf87ed5130ac8
Ihor Okhrimenko <iokhrime@redhat.com>
2018-04-01 20:30:24 +0300

    Working prototipe

--- printColorText.sh
+++ printColorText.sh
@@ -1,67 +1,121 @@
 #!/bin/bash
-correctCountOfArguments=2
+correctCountOfArguments=3
 countOfArguments=$#
-color=$1
-text=$2
-listOfArguments=( -b -r -g -y )
+textColor=$1
+backgroundColor=$2
+text=$3
+colorsList=( -d -r -g -y )
+
+DEFAULT="\e[39m"
+RED="\033[0;31m"
+GREEN="\033[32m"
+YELLOW="\033[1;33m"
+
+DEFAULT_BACKGROUND="\e[49m"
+RED_BACKGROUND="\e[41m"
+GREEN_BACKGROUND="\e[42m"
+YELLOW_BACKGROUND="\e[43m"
+
 
 
 help() {
 echo -e "Please enter one of the next arguments: \n
-         \t-b \tPrints inputed text in bold mode\n
-         \t-r \tPrints inputed text in red color\n
-         \t-g \tPrints inputed text in green color\n
-         \t-y \tPrints inputed text in yellow color\n"
+         \t-d \tPrints text or background in default color\n
+         \t-r \tPrints text or background in red color\n
+         \t-g \tPrints text or background in green color\n
+         \t-y \tPrints text or background in yellow color\n"
 }
 
 
 
 checkArgumentsCount() {
 if [[ $countOfArguments -ne $correctCountOfArguments ]]; then
-        echo -e "\n\nWrong arguments count. Should be $correctCountOfArguments arguments, but current count is $countOfArguments\n\n"
+        echo "Wrong arguments count. Should be $correctCountOfArguments arguments, but current count is $countOfArguments"
+        exit
     else
-        echo -e "\n\nArguments count is right\n\n"
+        echo "Arguments count is right"
     fi
 }
 
 
-
-checkArgumentsValid() {
-if [[ " ${listOfArguments[@]} " =~ " ${color} " ]]; then
-    return 0;
+isTextColorArgumentValid() {
+if [[ " ${colorsList[@]} " =~ " ${textColor} " ]]; then
+    echo "Text color argument is right"
 else
-    return 1;
-fi
-}
-
-
-
-printIfTrue() {
-if checkArgumentsValid ; then
-    echo -e "\n\nSpecified argument is valid\n\n"
-else
-    echo -e "\n\nSorry but inputed argument is wrong\n\n"
+    echo "Sorry, but text color argument is wrong"
     help
+    exit
 fi
 }
 
 
+isBackgroundColorArgumentValid() {
+if [[ " ${colorsList[@]} " =~ " ${backgroundColor} " ]]; then
+    echo "Background color argument is right"
+else
+    echo "Sorry but background color argument is wrong"
+    help
+    exit
+fi
+}
 
-colorized() {
-echo -e "\033[37;1;41m $text \033[0m"
+
+getTextColor() {
+case $textColor in
+    "-d")
+    echo "${DEFAULT}"
+    ;;
+
+    "-r")
+    echo "${RED}"
+    ;;
+
+    "-g")
+    echo "${GREEN}"
+    ;;
+
+    "-y")
+    echo "${YELLOW}"
+    ;;
+
+esac
+}
+
+
+getBackgroundColor() {
+case $backgroundColor in
+    "-d")
+    echo "${DEFAULT_BACKGROUND}"
+    ;;
+
+    "-r")
+    echo "${RED_BACKGROUND}"
+    ;;
+
+    "-g")
+    echo "${GREEN_BACKGROUND}"
+    ;;
+
+    "-y")
+    echo "${YELLOW_BACKGROUND}"
+    ;;
+
+esac
+}
 
 
 
-
+print() {
+echo -e "$(getTextColor)$(getBackgroundColor)$text$DEFAULT_BACKGROUND"
 }
 
 
 
 clear
 checkArgumentsCount
-#checkArgumentsValid
-printIfTrue
-colorized
-#help
+isTextColorArgumentValid
+isBackgroundColorArgumentValid
+#defineTextColor
+print
 
 
